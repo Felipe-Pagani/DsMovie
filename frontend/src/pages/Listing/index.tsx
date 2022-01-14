4d6a7d962e2b7d13e973dae4b4c41c41f0f4b1f1
Felipe Pagani <feh.pagani@gmail.com>
2022-01-14 19:54:12 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,23 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utills/requests";
 
 function Listing() {
+
+    //CORRETA
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(reponse => {
+        console.log(reponse.data);
+
+    });
+
+    /*FORMA ERRADA
+    axios.get("http://localhost:8080/movies?size=12&page=0")
+    .then(response => {
+        console.log(response.data);
+    });
+    */
     return (
         <>
             <Pagination />
