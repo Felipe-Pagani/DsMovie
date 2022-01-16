2c5648e809b82d445fdc5f69ec1844f46dc47bd4
Felipe Pagani <feh.pagani@gmail.com>
2022-01-16 12:49:17 -0300

    Alter Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -9,7 +9,7 @@ function Navbar(){
                 <a href="https://github.com/Felipe-Pagani">
                     <div className="dsmovie-contact-container">  
                         <GithubIcon />
-                        <p className="dsmovie-contact-link">/devsuperior</p>
+                        <p className="dsmovie-contact-link">/Felipe-Pagani</p>
                     </div>
                 </a>
             </div> 
